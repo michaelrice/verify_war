3a8993c934184629e775c64f620365f25c24a357
Michael Rice <michael.rice@rackspace.com>
2014-03-19 23:24:55 -0500

    initial commit

---
+++ verify_war.py
@@ -0,0 +1,27 @@
+import sys
+import zipfile
+from os.path import isfile
+from os.path import expanduser
+
+__author__ = 'errr'
+
+
+class FileNotFoundException(Exception):
+    pass
+
+
+def find_config(war_file=None):
+    war_to_check = expanduser(war_file)
+    if isfile(war_to_check) and zipfile.is_zipfile(war_to_check):
+        zf = zipfile.ZipFile(war_to_check, 'r')
+        file_list = zf.namelist()
+        if "WEB-INF/classes/external-config.groovy" in file_list:
+            raise SystemExit
+    
+    raise FileNotFoundException("The war file could not be found, or was not a valid zipfile.")
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        raise Exception("This script only support 1 arg. That is the full path to the war file.")
+    war_file = sys.argv[1]
+    find_config(war_file)
